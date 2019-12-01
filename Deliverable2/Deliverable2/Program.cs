417644fd800fcce9f52fef3ae178fc1aef753143
Dylan Rule <dylanmrule@yahoo.com>
2019-12-01 17:25:13 -0500

    Deliverable2

---
+++ Deliverable2/Deliverable2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("What is your message?");
+        string input = Console.ReadLine();
+
+        input = input.ToUpper();
+
+        int checksum = 0;
+        string message = null;
+        foreach (char c in input)
+        {
+            if (message != null)
+                message += "-";
+            else
+                message = "";
+
+            message += c - 64;
+            checksum += c;
+        }
+        Console.WriteLine("Your encoded message is: " + message);
+        Console.WriteLine("Your checksum total is: " + checksum);
+    }
+}
